8791b95c248c13e42cc0a6d1ecfd12989a610ae8
Omar Ortiz <omaredvega@gmail.com>
2026-03-21 14:45:04 -0400

    first commit

---
+++ backend/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using RegistroApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
---
+++ backend/RegistroApi/Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace RegistroApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+    }
+}
